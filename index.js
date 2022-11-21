41addc0757b57797d32f5c38775b940a929d8cdd
Lien Nguyen <lienlam217@gmail.com>
2022-11-21 15:51:57 -0600

    commit tree 9a782d041d05e5f5ee277e2c29d74cd06727b143

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'SASSY HOPPER';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser + '!'
+const shortGreeting = "Welcome, S!"
